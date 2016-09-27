aa9bb3c484d68af46412f3c492aa8fb4c7d7d85c
atw-yamamoto <masahira@atware.co.jp>
2016-09-27 23:53:11 +0900

    Added nightmare

--- execute.js
+++ execute.js
@@ -1,17 +1,16 @@
-/**
- * Created by masahirayamamoto on 2016/09/08.
- */
-
 var service = require('./app/service/execute.js');
-var nightmare = require('./nighmare.js');
+var __ = require('underscore');
+//var nightmare = require('./nightmare.js');
 
-function getNightmare() {
-    nightmare.getMoneyFromUcho();
-}
+//function getNightmare() {
+//    nightmare.getMoneyFromUcho();
+//}
 
-function output(argument) {
-    console.log(argument);
+//function output(argument) {
+//    console.log(argument);
 //    console.log("とおっていますね");
 //    service.addedPayMoney("massan",argument);
-}
+//}
 
+
+service.addedPayMoney("massan", __.random(0, 100));
---
+++ nightmare.js
@@ -0,0 +1,23 @@
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({'show': false});
+var Config = require('./config.js');
+//var service = require('./app/service/execute.js');
+
+nightmare
+    .goto(Config.AccessConfig.targetUrl)
+    .type('#userid', Config.AccessConfig.userId)
+    .type('#password', Config.AccessConfig.userPass)
+    .click("input[type=\"image\"]")
+    .wait('#vp_alcor_view_Label_7')
+    .evaluate(function () {
+        console.log("hello");
+        return document.querySelector('#vp_alcor_view_Label_7').textContent
+    })
+    .end()
+    .then(function (result) {
+        console.log("ここか....");
+        console.log(result);
+    })
+    .catch(function (error) {
+        console.error('Search failed:', error);
+    });
